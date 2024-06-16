912830574764768eb14249e725dab389d12da816
olgaivanovagaula <olgaivanova2512@yandex.ru>
2024-06-16 22:17:56 +0300

    создание и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+    const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+    const cardContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+    function createCard(item, removeCard) {
+        const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+        const deleteButton = card.querySelector('.card__delete-button');
+        const cardImage = card.querySelector('.card__image');
+        cardImage.src = item.link;
+        cardImage.alt = item.name;
+        card.querySelector('.card__title').textContent = item.name;
+        
 
+        deleteButton.addEventListener('click', removeCard);
+
+        //cardContainer.append(card);
+        return card;
+
+    }
 // @todo: Функция удаления карточки
-
+    function deleteCard(evt) {
+        evt.target.closest('.places__item').remove();
+    }
 // @todo: Вывести карточки на страницу
+    initialCards.forEach(function(item) {
+        const card = createCard(item, deleteCard);
+        cardContainer.append(card);
+    })
